dcaca0a795bf6c7ed9cb9699fb87629ee980ce76
Nikhil Patteri <nikhilpatteri@gmail.com>
2022-03-28 02:11:11 +0530

    initial commit for port change

--- src/gfk-root-config.ts
+++ src/gfk-root-config.ts
@@ -4,15 +4,21 @@ registerApplication({
   name: "@gfk/single-spa-navbar",
   app: () =>
     System.import(
-      "//localhost:8081/dist/gfk-single-spa-navbar.js"
+      "//localhost:8080/dist/gfk-single-spa-navbar.js"
     ),
   activeWhen: ["/"],
 });
 
 registerApplication({
   name: "@gfk/crosstab",
-  app: () => System.import("//localhost:8080/dist/gfk-crosstab.js"),
+  app: () => System.import("//localhost:8081/dist/gfk-crosstab.js"),
   activeWhen: ["/crosstab"]
+});
+
+registerApplication({
+  name: "@gfk/catalyst",
+  app: () => System.import("//localhost:8082/dist/gfk-catalyst.js"),
+  activeWhen: ["/catalyst"]
 });
 
 start({
